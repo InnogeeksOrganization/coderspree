4ce69688a08b9fcadbd7f46238c66c049a23fe98
Tushar <codingid6@gmail.com>
2021-09-27 18:33:01 +0530

    new added

--- check_submissions.py
+++ check_submissions.py
@@ -12,7 +12,6 @@ home = os.path.abspath(Path(__file__).parent)
 submission_architecture = {
     "GettingStarted": 5,
     "Patterns": 2,
-    "FunctionsAndArrays": 10
 }
 
 domains = ["AR-VR", "IOT", "ML", "Android", "Web"]
