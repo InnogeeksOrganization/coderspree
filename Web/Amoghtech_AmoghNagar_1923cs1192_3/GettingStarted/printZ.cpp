5bbe150349815a4407ac5c9e5edd587791bb2134
Amoghtech <amogh.1923cs1192@kiet.edu>
2021-09-30 21:53:17 +0530

    deleted empoty files

--- Web/Amoghtech_AmoghNagar_1923cs1192_3/GettingStarted/printZ.cpp
+++ Web/Amoghtech_AmoghNagar_1923cs1192_3/GettingStarted/printZ.cpp
@@ -1,7 +1,11 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(int argc, char** agrc){
+int main(int argc, char **agrc)
+{
     //write your code here
-    cout<<"*****"<<endl;cout<<"   * "<<endl;cout<<"  *  "<<endl;cout<<" *   "<<endl;cout<<"*****"<<endl;
-
+    cout << "*****" << endl;
+    cout << "   * " << endl;
+    cout << "  *  " << endl;
+    cout << " *   " << endl;
+    cout << "*****" << endl;
 }
