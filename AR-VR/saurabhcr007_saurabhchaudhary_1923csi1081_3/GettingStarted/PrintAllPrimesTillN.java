1f27357a29115ad212e4497eb86846e081b1a166
saurabh chuadhary <supersaurabh017@gmail.com>
2021-09-30 20:42:01 +0530

    java program

--- AR-VR/saurabhcr007_saurabhchaudhary_1923csi1081_3/GettingStarted/PrintAllPrimesTillN.java
+++ AR-VR/saurabhcr007_saurabhchaudhary_1923csi1081_3/GettingStarted/PrintAllPrimesTillN.java
@@ -2,25 +2,23 @@ import java.util.*;
 
 public class PrintAllPrimesTillN {
     public static void main(String[] args) {
-        Scanner scn=new Scanner(System.in);
-        int count=0;
+        Scanner scn = new Scanner(System.in);
         int low=scn.nextInt();
         int high=scn.nextInt();
+        int count;
         for(int i=low;i<=high;i++){
-            if(i==0 || i==1){
-                count=count+1;
-            }
-            else{
-                for(int j=2;j<i;j++){
-                    if(i%j==0){
-                        count=count+1;
-                    }
+            count=0;
+            for(int j=2;j*j<i;j++){
+                if(i%j==0){
+                    count++;
+                }
+                if(count>0){
+                    break;
                 }
             }
             if(count==0){
                 System.out.println(i);
             }
-            count=0;
         }
     }
 }
