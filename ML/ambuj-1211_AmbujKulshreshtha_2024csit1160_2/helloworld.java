57a3eeccfc7d1cd8de2e23d77f1dcb9f6db05075
Ambuj kulshreshtha <kulshreshthaak.12@gmail.com>
2021-10-01 15:12:00 +0530

    Update helloworld.java

--- ML/ambuj-1211_AmbujKulshreshtha_2024csit1160_2/helloworld.java
+++ ML/ambuj-1211_AmbujKulshreshtha_2024csit1160_2/helloworld.java
@@ -1,5 +1,8 @@
 public class helloworld{
     public static void main(String[] args){
-    System.out.println("hello world");
+    int n=10;
+    for(int i=0;i<n;i++){
+        System.out.println("hello world");
+    }
     }
 }
