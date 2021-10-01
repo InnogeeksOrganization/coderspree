93ba6f4600210250262b9293c075f63c8e65f857
Amoghtech <amogh.1923cs1192@kiet.edu>
2021-10-01 19:05:43 +0530

    update

--- Web/Amoghtech_AmoghNagar_1923cs1192_3/GettingStarted/Inverse-a-number.cpp
+++ Web/Amoghtech_AmoghNagar_1923cs1192_3/GettingStarted/Inverse-a-number.cpp
@@ -2,37 +2,36 @@
 
 using namespace std;
 
+bool sortcompare(pair<int,int>& a,pair<int,int>& b){
+    return a.second<b.second;
+}
+
+
 int main(int argc, char **argv)
 {
     int n;
     cin >> n;
-    map<int, pair<pair<int, int>, int>> m;
     long long int t = 10;
-    int x = n, c = 0;
+    int x = n, c = 0,m=0;
     while (x != 0)
     {
         c++;
         x /= 10;
     }
     x = 1;
+    m=c;
+    pair<int,int> p[c+1];
+    c++;
     while (c--)
     {
-        m.insert(make_pair(x, make_pair(make_pair(x, (n % t) * 10 / t), 0)));
+        p[x].first=x;
+        p[x].second=(n % t) * 10 / t;
         t *= 10;
         x++;
     }
-    for (auto i : m)
-    {
-        // cout << i.first << " " << i.second.first << " " < < < < endl;
-        if (i.second.second == 0)
-        {
-            int l = i.second.first;
-            int a = i.first;
-            int b = m[l].first;
-            i.first = b;
-            m[l].first = a;
-            m[l].second = 1;
-            i.second.second = 1;
-        }
+    
+    sort(p,p+m+1,sortcompare);
+    for(int i=m;i>=1;i--){
+        cout<<p[i].first;
     }
 }
