fe95cb6f510b6b6019118a27c557b6b4afcef7f4
Ayushsingh07 <nqu7069@gmail.com>
2021-10-02 15:21:37 +0530

    added new

--- ML/Ayushsingh07_Ayushkumarsingh_2024cse1037_2/gettingstarted/primefactors.py
+++ ML/Ayushsingh07_Ayushkumarsingh_2024cse1037_2/gettingstarted/primefactors.py
@@ -1,12 +1,12 @@
 import math
 n=int(input())
 while n%2==0:
-    print(2)
+    print(2,end=" ")
     n=n/2
 for i in range(3,int(math.sqrt(n))+1,2):
     while (n % i == 0):
-         print (i)
+         print (int(i),end=" ")
          n = n / i
     
 if n>2:
-    print (n)
+    print (int(n),end=" ")
