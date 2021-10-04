e0c5d6d39c933d5f469b6455e255091f0c370526
Shatanshu <shatanshu.1923csi1044@kiet.edu>
2021-10-04 22:01:51 +0530

    RotateNumber

---
+++ AR-VR/shatanshu123_ShatanshuAgarwal_1923csi1044_3/GettingStarted/RotateNumber.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(){
+    int n,k;
+    cin>>n>>k;
+    int temp = n;
+    int digits = 0;
+    while(temp != 0){
+        temp /= 10;
+        digits++;
+    }
+    k = k % digits;
+    if(k < 0){
+        k += digits;
+    }
+    int div = (int)(pow(10,k));
+    int multiplier = (int)(pow(10,digits - k));
+    int fh = n / div;
+    int sh = n % div;
+    int rot = sh * multiplier + fh;
+    cout<<rot;
+    return 0;
+}
