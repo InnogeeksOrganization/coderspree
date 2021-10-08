03849c90c217cb66e6eefd05b3c059d309808439
s-inghvaibhav <singh17vaibhav@gmail.com>
2021-10-08 18:13:24 +0530

    first program

---
+++ Android/s-inghvaibhav_VaibhavSingh_2024it1030_2/GettingStarted/print_z.java
@@ -0,0 +1,13 @@
+package gettingStarted;
+
+public class print_z {
+    public static void main(String[] args) {
+
+        System.out.println("*****");
+        System.out.println("   * ");
+        System.out.println("  *  ");
+        System.out.println(" *   ");
+        System.out.println("*****");
+
+    }
+}
