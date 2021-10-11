eec86ec1733826972e3740e7e9e2185cefb9bb03
Anshul-13J <74321084+Anshul-13J@users.noreply.github.com>
2021-10-12 00:48:12 +0530

    Fixed
    Due to some wrong edits, I think I edited someone else's code, so I fixed it back.

--- AR-VR/thisiskartikgupta_KartikGupta_1923co1043_3/GettingStarted/IsANumberPrime.java
+++ AR-VR/thisiskartikgupta_KartikGupta_1923co1043_3/GettingStarted/IsANumberPrime.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class isANumberPrime {
+public class IsANumberPrime {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     int t = sc.nextInt();
