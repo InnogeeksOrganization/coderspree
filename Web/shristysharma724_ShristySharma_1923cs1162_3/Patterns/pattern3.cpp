c19d24c058c2a29716412ad9d337750caed23737
Shristy <shristysharma724@gmail.com>
2021-10-14 19:42:35 +0530

    update pattern3

--- Web/shristysharma724_ShristySharma_1923cs1162_3/Patterns/pattern3.cpp
+++ Web/shristysharma724_ShristySharma_1923cs1162_3/Patterns/pattern3.cpp
@@ -9,9 +9,12 @@ int main(int argc, char **argv)
 
      for (int i = 0; i < n; i++)
     {
-       for (int j = 0; j < (n- i); j++)
+       for (int j = 0; j < n; j++)
        {
+           if(i+j>=(n-1))
            cout<<"*";
+           else 
+           cout<<" ";
        }
        cout<<endl;
     }
