748969bc7ced441bea1c85833a9a8ec045d81ec5
Arjit Goyal <75082382+arjit1704@users.noreply.github.com>
2021-10-22 22:55:41 +0530

    Add files via upload

---
+++ Web/arjit1704_Arjit_2024csit1132_2/Prime.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(int argc, char **argv){
+    int i,n,t;
+    cin >> t;
+    for(i=0;i<=n;i++){
+        cin>>n;
+    }
+    for(n=1;n<=t;n++){
+    if(n==1||n==0){
+        cout<<"not prime\n";
+    }
+    else if(n==2){
+        cout<<"prime\n"4;
+    }
+    else if(n%i==0){
+        cout<<"not prime\n";
+    }
+    else{
+        cout<<"prime\n";
+    }  
+    }
+}
