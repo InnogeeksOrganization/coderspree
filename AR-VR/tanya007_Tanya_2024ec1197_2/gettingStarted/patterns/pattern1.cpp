bdba0b25255e79db24429fc40442413c1a77dbbd
tanya007-github <86552981+tanya007-github@users.noreply.github.com>
2021-10-26 01:48:32 +0530

    pattern type 1

---
+++ AR-VR/tanya007_Tanya_2024ec1197_2/gettingStarted/patterns/pattern1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int r;
+    cin>>r;
+    for(int i=0;i<r;i++){
+        for(int j=0;j<=i;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    
+}
