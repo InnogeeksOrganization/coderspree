12645ac31b82b3053edf215b3e57a404a230ecd0
Aviral Srivastava <aviralji4@gmail.com>
2021-11-16 16:11:16 +0530

    Arrays and Patterns completed

--- Web/godofgeeks23_AviralSrivastava_2024cse1056_2/FunctionAndArrays/anybaseaddition.cpp
+++ Web/godofgeeks23_AviralSrivastava_2024cse1056_2/FunctionAndArrays/anybaseaddition.cpp
@@ -1,8 +1,8 @@
-#include <iostream>
+#include<iostream>
+
 using namespace std;
 int AnyToDec(int n, int base)
 {
-    //write your code here.
     int decimalNumber = 0;
     int baseNumber = 1;
     while (n != 0)
@@ -14,12 +14,19 @@ int AnyToDec(int n, int base)
     }
     return decimalNumber;
 }
-int main()
-{
-    int n;
-    int b;
-    cin >> n;
-    cin >> b;
-    int res = AnyToDec(n, b);
-    cout << res << endl;
+int getSum(int b, int n1, int n2) {
+    int n = AnyToDec(n1, b) + AnyToDec(n2, b);
+    string s = "";
+    while (n > 0)
+    {
+        s = char(n % b + '0') + s;
+        n /= b;
+    }
+    cout << s << endl;
+}
+
+int main() {
+    int b, n1, n2;
+    cin >> b >> n1 >> n2;
+    getSum(b, n1, n2);
 }
