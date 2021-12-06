958fac5c024417ab014c0ea874747eb3acb0339a
Paras Gupta <guptaparas039@gmail.com>
2021-12-06 18:48:59 +0000

    Add solution for merge-sort-official

---
+++ ML/g-paras_ParasGupta_1923ec1161_3/TimeAndSpaceComplexity/merge-sort-official.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+
+#include <vector>
+
+using namespace std;
+
+void input(vector<int> &arr)
+{
+    for (int i = 0; i < arr.size(); i++)
+    {
+        cin >> arr[i];
+    }
+}
+
+void print(vector<int> &arr)
+{
+    for (int i = 0; i < arr.size(); i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+vector<int> mergeTwoSortedArrays(vector<int> &A, vector<int> &B)
+{
+    if (A.size() == 0 || B.size() == 0)
+        return A.size() == 0 ? B : A;
+
+    int n = A.size();
+    int m = B.size();
+    vector<int> ans(n + m, 0);
+
+    cout << ("Merging these two arrays ") << endl;
+    cout << ("left array -> ");
+    print(A);
+    cout << ("right array -> ");
+    print(B);
+
+    int i = 0, j = 0, k = 0;
+    while (i < n && j < m)
+    {
+        if (A[i] < B[j])
+            ans[k++] = A[i++];
+        else
+            ans[k++] = B[j++];
+    }
+
+    while (i < n)
+        ans[k++] = A[i++];
+    while (j < m)
+        ans[k++] = B[j++];
+
+    return ans;
+}
+
+vector<int> mergeSort(vector<int> &arr, int lo, int hi)
+{
+    if (lo == hi)
+    {
+        vector<int> ba;
+        ba.push_back(arr[lo]);
+        return ba;
+    }
+    int mid = (lo + hi) / 2;
+    vector<int> lh = mergeSort(arr, lo, mid);
+    vector<int> uh = mergeSort(arr, mid + 1, hi);
+    vector<int> ans = mergeTwoSortedArrays(lh, uh);
+    return ans;
+}
+
+int main()
+{
+    int n, m;
+    cin >> n;
+    vector<int> A(n, 0);
+    input(A);
+
+    vector<int> ans = mergeSort(A, 0, n - 1);
+    cout << "Sorted Array -> ";
+    print(ans);
+    return 0;
+}
