ecf62f87e627f33e259e62554192e39d884797b8
Paras Gupta <guptaparas039@gmail.com>
2021-12-12 22:39:36 +0530

    design pattern 6 soln

--- ML/g-paras_ParasGupta_1923ec1161_3/patterns/design-pattern-6-official.cpp
+++ ML/g-paras_ParasGupta_1923ec1161_3/patterns/design-pattern-6-official.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main()
+{
     int n;
     cin >> n;
 
-    for(int i=0; i<n; i++) {
-        for(int j=0; j<)
-    }
+    // for(int i=0; i<n; i++) {
+    //     for(int j=0; j<)
+    // }
 }
