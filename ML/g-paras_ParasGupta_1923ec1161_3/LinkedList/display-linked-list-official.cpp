e943da9a93ce0b86d9a92f86e3677cc1f4543710
Paras Gupta <guptaparas039@gmail.com>
2022-01-10 03:46:23 +0000

    Add solution for display-linked-list-official

---
+++ ML/g-paras_ParasGupta_1923ec1161_3/LinkedList/display-linked-list-official.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *next;
+};
+
+class linked_list
+{
+public:
+    node *head, *tail;
+    int size = 0;
+
+public:
+    linked_list()
+    {
+        head = NULL;
+        tail = NULL;
+    }
+
+    void addLast(int n)
+    {
+        node *tmp = new node;
+        tmp->data = n;
+        tmp->next = NULL;
+
+        if (head == NULL)
+        {
+            head = tmp;
+            tail = tmp;
+        }
+        else
+        {
+            tail->next = tmp;
+            tail = tail->next;
+        }
+        size++;
+    }
+    void display()
+    {
+        for (node *temp = head; temp != NULL; temp = temp->next)
+        {
+            cout << temp->data << " ";
+        }
+        if (size != 0)
+            cout << "\n";
+    }
+
+    void testList()
+    {
+        for (node *temp = head; temp != NULL; temp = temp->next)
+        {
+            cout << temp->data << endl;
+        }
+        cout << size << endl;
+
+        if (size > 0)
+        {
+            cout << tail->data << endl;
+        }
+    }
+};
+
+int main()
+{
+
+    string str;
+    linked_list l;
+    while (true)
+    {
+        getline(cin, str);
+        if (str[0] == 'q')
+        {
+            break;
+        }
+        else if (str[0] == 'a')
+        {
+            string ss = str.substr(8, 2);
+            int n = stoi(ss);
+            l.addLast(n);
+        }
+        else if (str[0] == 's')
+        {
+            cout << l.size << endl;
+        }
+        else if (str[0] == 'd')
+        {
+            l.display();
+        }
+    }
+
+    return 0;
+}
