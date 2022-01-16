18bd3b662c33475fbf6925175c3908ec05485425
Paras Gupta <guptaparas039@gmail.com>
2022-01-16 10:49:56 +0000

    Add solution for generic-tree-traversal-official

---
+++ ML/g-paras_ParasGupta_1923ec1161_3/GenericTree/generic-tree-traversal-official.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct Node
+{
+    int data;
+    vector<Node *> children;
+};
+
+Node *newNode(int key)
+{
+    Node *temp = new Node;
+    temp->data = key;
+    return temp;
+}
+
+Node *construct(int arr[], int n)
+{
+    Node *root = NULL;
+    stack<Node *> st;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] == -1)
+        {
+            st.pop();
+        }
+        else
+        {
+            Node *t = newNode(arr[i]);
+            if (st.size() > 0)
+            {
+                st.top()->children.push_back(t);
+            }
+            else
+            {
+                root = t;
+            }
+            st.push(t);
+        }
+    }
+    return root;
+}
+
+void traversals(Node *node)
+{
+    if (node == NULL)
+    {
+        return;
+    }
+    cout << "Node Pre " << node->data << endl;
+    for (auto i : node->children)
+    {
+        cout << "Edge Pre " << node->data << "--" << i->data << endl;
+        traversals(i);
+        cout << "Edge Post " << node->data << "--" << i->data << endl;
+    }
+    cout << "Node Post " << node->data << endl;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    Node *root = construct(arr, n);
+    traversals(root);
+}
