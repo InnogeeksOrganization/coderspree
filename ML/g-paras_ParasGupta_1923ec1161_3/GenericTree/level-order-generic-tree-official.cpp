5bfe043255dbb613692b0cf758761c83cd245d57
Paras Gupta <guptaparas039@gmail.com>
2022-01-20 03:21:18 +0000

    Add solution for level-order-generic-tree-official

---
+++ ML/g-paras_ParasGupta_1923ec1161_3/GenericTree/level-order-generic-tree-official.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct Node
+{
+    int data;
+    vector<Node *> children;
+};
+
+Node *newNode(int key)
+{
+    Node *temp = new Node;
+    temp->data = key;
+    return temp;
+}
+
+Node *construct(int arr[], int n)
+{
+    Node *root = NULL;
+    stack<Node *> st;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] == -1)
+        {
+            st.pop();
+        }
+        else
+        {
+            Node *t = newNode(arr[i]);
+            if (st.size() > 0)
+            {
+                st.top()->children.push_back(t);
+            }
+            else
+            {
+                root = t;
+            }
+            st.push(t);
+        }
+    }
+    return root;
+}
+
+void levelorder(Node *node)
+{
+    queue<Node *> q;
+    q.push(node);
+    while (!q.empty())
+    {
+        Node *temp = q.front();
+        q.pop();
+        cout << temp->data << " ";
+        for (auto i : temp->children)
+            q.push(i);
+    }
+    cout << "." << endl;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    Node *root = construct(arr, n);
+    levelorder(root);
+}
