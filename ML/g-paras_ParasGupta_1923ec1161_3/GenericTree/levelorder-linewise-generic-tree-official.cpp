4f8ff712e82ce1aa36fc328f652604040b2a655f
Paras Gupta <guptaparas039@gmail.com>
2022-01-21 11:06:23 +0000

    Add solution for levelorder-linewise-generic-tree-official

---
+++ ML/g-paras_ParasGupta_1923ec1161_3/GenericTree/levelorder-linewise-generic-tree-official.cpp
@@ -0,0 +1,119 @@
+#include <bits/stdc++.h>
+#include <climits>
+using namespace std;
+
+class Node
+
+{
+
+public:
+    int data = 0;
+
+    vector<Node *> children;
+
+    Node(int data)
+
+    {
+
+        this->data = data;
+    }
+};
+
+void display(Node *node)
+{
+    string s = "";
+    s += to_string(node->data) + " -> ";
+    for (Node *child : node->children)
+    {
+        s += to_string(child->data) + ", ";
+    }
+
+    cout << s << "." << endl;
+
+    for (Node *child : node->children)
+    {
+        display(child);
+    }
+}
+
+Node *constructor01(vector<int> &arr)
+
+{
+
+    if (arr.size() == 0)
+
+        return NULL;
+
+    vector<Node *> stack;
+
+    stack.push_back(new Node(arr[0]));
+
+    Node *root = stack[0];
+
+    for (int i = 1; i < arr.size(); i++)
+
+    {
+
+        if (arr[i] != -1)
+
+        {
+
+            Node *node = stack.back();
+
+            Node *nnode = new Node(arr[i]);
+
+            node->children.push_back(nnode);
+
+            stack.push_back(nnode);
+        }
+
+        else
+
+            stack.pop_back();
+    }
+
+    return root;
+}
+
+Node *linearize(Node *node)
+{
+    // queue<Node*> q;
+    vector<Node *> q;
+    q.push_back(node);
+
+    while (!q.empty())
+    {
+        for (auto i : q)
+        {
+            cout << i->data << " ";
+        }
+        // queue<Node*> n;
+        vector<Node *> n;
+        for (auto i : q)
+            for (auto j : i->children)
+                n.push_back(j);
+        cout << endl;
+        q = n;
+    }
+}
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    Node *root = constructor01(arr);
+
+    linearize(root);
+    //   display(root);
+}
+
+int main()
+{
+    solve();
+    return 0;
+}
