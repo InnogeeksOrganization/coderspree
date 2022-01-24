d978697091e5b68c71bc2fe3adf5a021d9df69e1
Paras Gupta <guptaparas039@gmail.com>
2022-01-24 10:32:05 +0000

    Add solution for reverse-an-array-official

---
+++ ML/g-paras_ParasGupta_1923ec1161_3/FunctionAndArrays/reverse-an-array-official.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+void reverse(int *arr, int n)
+{
+    int start = 0;
+    int end = n - 1;
+    while (start < end)
+    {
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
+    }
+}
+
+void display(int *arr, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int *arr = new int[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    reverse(arr, n);
+    display(arr, n);
+}
