b93d8ae47c68dbd158048053269e48ad4baefb68
Paras Gupta <guptaparas039@gmail.com>
2022-02-01 19:34:20 +0530

    Solution for generic tree size

---
+++ ML/g-paras_ParasGupta_1923ec1161_3/GenericTree/size-generic-tree-official.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct Node
+{
+    int data;
+    vector<Node *> children;
+};
+
+Node *newNode(int key)
+{
+    Node *temp = new Node;
+    temp->data = key;
+    return temp;
+}
+
+Node *construct(int arr[], int n)
+{
+    Node *root = NULL;
+    stack<Node *> st;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] == -1)
+        {
+            st.pop();
+        }
+        else
+        {
+            Node *t = newNode(arr[i]);
+            if (st.size() > 0)
+            {
+                st.top()->children.push_back(t);
+            }
+            else
+            {
+                root = t;
+            }
+            st.push(t);
+        }
+    }
+    return root;
+}
+
+int size(Node *node)
+{
+    if (node == NULL)
+        return 0;
+    int count = 0;
+    for (auto i : node->children)
+    {
+        count++;
+        count += size(i);
+    }
+    return count;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    Node *root = construct(arr, n);
+    cout << size(root) + 1 << endl;
+}
